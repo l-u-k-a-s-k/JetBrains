2061ace77804931d119b1c6adbf76bb11765acc9
l-u-k-a-s-k <lukas1@poczta.fm>
2020-07-21 02:43:58 +0200

    finished stage with itertools product

--- hack.py
+++ hack.py
@@ -1,3 +1,25 @@
+import sys
 import socket
+import itertools
 
-pass
+
+args = sys.argv
+conn = socket.socket()
+conn.connect((args[1], int(args[2])))
+
+c = ''.join([chr(c) for c in range(97, 97 + 26)] + [str(c) for c in range(10)])
+pass_dict = []
+password = ''
+while not password:
+    pass_dict.append(c)
+
+    for p in itertools.product(*pass_dict):
+        pass_to_try = ''.join(p)
+        conn.send(pass_to_try.encode())
+        serv_resp = conn.recv(50).decode()
+        if serv_resp == 'Connection success!':
+            password = pass_to_try
+            print(password)
+            break
+
+conn.close()
